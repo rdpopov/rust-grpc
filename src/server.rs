0d796ce5bd5bb5d1352545bdc2d41b977b3e1495
Rosen Popov <rdonkow@gmail.com>
2022-01-11 14:48:18 +0200

    WIP

--- src/server.rs
+++ src/server.rs
@@ -13,17 +13,27 @@ pub mod hello_world {
 
 #[derive(Debug)]
 pub struct MyGreeter {
-    db: Mutex<Box<Connection>>,
+    db: Mutex<Connection>,
+    // db: Mutex<Box<Connection>>,
 }
+
+struct SongEntry {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>,
+}
+
 
 #[tonic::async_trait]
 impl Greeter for MyGreeter {
     // This Method implements adding data to the db
     async fn add_meta(&self, request: Request<SongMeta>) -> Result<Response<AddResult>, Status> {
         let req = request.into_inner();
+        // NOTE: this is iffy does the mutex drop the guard when the request is done?
+        // or do i have to free it>
         let res = self.db.lock().unwrap().execute(
-            "INSERT INTO songs (sname, artist, album, artwork, lyrics) VALUES (?1, ?2, ?3, ?4, ?5)",
-            params![req.name, req.artist, req.album, req.artwork, req.lyrics],
+            "INSERT INTO songs (fname, name, artist, album, artwork, lyrics) VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+            params![req.fname ,req.name, req.artist, req.album, req.artwork, req.lyrics],
         );
         let reply = hello_world::AddResult {
             result: format!("Done {}", res.unwrap()),
@@ -33,6 +43,11 @@ impl Greeter for MyGreeter {
     async fn query_meta(&self, request: Request<SongName>) -> Result<Response<SongMeta>, Status> {
         println!("Got a request: {:?}", request);
         // TODO: do something with request.name
+        let query = "SELECT name, artist, album, artwork, lyrics FROM songs WHERE songs.fname = {}".format(request.into_inner().song_name);
+
+        let db = self.db.lock().unwrap();
+        let stmt = db.prepare(&query);
+
 
         let reply = hello_world::SongMeta {
             name: String::from("name of song"),
@@ -50,7 +65,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
     // TODO: add a data structure for song meta and create table
     let greeter = MyGreeter {
-        db: Mutex::new(Box::new(Connection::open("song_meta_db.db").unwrap())), // hope it does not break ...
+        db: Mutex::new(Connection::open("song_meta_db.db").unwrap()), // hope it does not break ...
     };
 
     Server::builder()

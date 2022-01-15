5849538cb79b4549c921e12f16a9e73ab8e78145
Rosen Popov <rdonkow@gmail.com>
2022-01-15 15:50:01 +0200

    Added a gitignore. Also now it works

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,6 @@
 use rusqlite::{params, Connection, Result};
 use std::sync::Mutex;
-use tonic::codegen::http::request;
+// use tonic::codegen::http::request;
 use tonic::{transport::Server, Request, Response, Status};
 
 use hello_world::greeter_server::{Greeter, GreeterServer};
@@ -14,23 +14,12 @@ pub mod hello_world {
 #[derive(Debug)]
 pub struct MyGreeter {
     db: Mutex<Connection>,
-    // db: Mutex<Box<Connection>>,
-}
-
-struct SongEntry {
-    id: i32,
-    name: String,
-    data: Option<Vec<u8>>,
 }
 
 #[tonic::async_trait]
 impl Greeter for MyGreeter {
-    // This Method implements adding data to the db
-    // NOTE: this works
     async fn add_meta(&self, request: Request<SongMeta>) -> Result<Response<AddResult>, Status> {
         let req = request.into_inner();
-        // NOTE: this is iffy does the mutex drop the guard when the request is done?
-        // or do i have to free it>
         let res = self.db.lock().unwrap().execute(
             "INSERT INTO songs (fname, name, artist, album, artwork, lyrics) VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
             params![req.fname ,req.name, req.artist, req.album, req.artwork, req.lyrics],
@@ -43,17 +32,14 @@ impl Greeter for MyGreeter {
 
     async fn query_meta(&self, request: Request<SongName>) -> Result<Response<SongMeta>, Status> {
         println!("Got a request: {:?}", request);
-        // TODO: do something with request.name
         let query = format!(
             "SELECT fname, name, artist, album, artwork, lyrics FROM songs WHERE fname = \"{}\"",
             request.into_inner().song_name
         );
-        println!("{}",query);
+        println!("{}", query);
 
         let db = self.db.lock().unwrap();
         let mut stmt = db.prepare(&query).unwrap();
-
-        // TODO: add a way to release the lock when a query fails
         let song_iter = stmt.query_map([], |row| {
             Ok(SongMeta {
                 fname: row.get(0)?,
@@ -64,16 +50,24 @@ impl Greeter for MyGreeter {
                 lyrics: row.get(5)?,
             })
         });
-
-        let res = song_iter.unwrap().next().unwrap().unwrap();
-        Ok(Response::new(res))
+        let res = song_iter.unwrap().next();
+        match res {
+            None => return Ok(Response::new(SongMeta {
+                fname: "None".to_string(),
+                name: "None".to_string(),
+                artist: "None".to_string(),
+                album: "None".to_string(),
+                artwork: "None".to_string(),
+                lyrics: "None".to_string(),
+            })),
+            _ => return  Ok(Response::new(res.unwrap().unwrap())),
+        };
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    // TODO: add a data structure for song meta and create table
     let greeter = MyGreeter {
         db: Mutex::new(Connection::open("song_meta_db.db").unwrap()), // hope it does not break ...
     };
@@ -85,7 +79,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             artist  TEXT NOT NULL,
             album   TEXT NOT NULL,
             artwork TEXT NOT NULL,
-            lyrics  TEXT NOT NULL)", [])?; 
+            lyrics  TEXT NOT NULL)",
+        [],
+    )?;
 
     println!("Running ... ");
 
@@ -93,6 +89,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .add_service(GreeterServer::new(greeter))
         .serve(addr)
         .await?;
-
     Ok(())
 }
